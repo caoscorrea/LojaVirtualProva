b51417fd49567df129691b9a8132057d8af94069
caoscorrea <carloseduardo.silva1805@gmail.com>
2025-06-09 16:25:41 -0300

    aqui eu terminei a parte do cadastro de produto

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using LojaVirtual.Models;
+
+class Program
+{
+    static void Main()
+    {
+        var produtos = new List<Produto>();
+
+        try
+        {
+            produtos.Add(new Produto(1, "Notebook", 3500, "Eletrônicos"));
+            produtos.Add(new Produto(2, "Camisa", 120, "Vestuário"));
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Erro ao cadastrar produto: {ex.Message}");
+        }
+
+        Console.WriteLine("Produtos cadastrados:");
+        foreach (var produto in produtos)
+        {
+            Console.WriteLine(produto);
+        }
+    }
+}
